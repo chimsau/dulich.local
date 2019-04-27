c6f6435697a84256af900d2701a8d8942987ea5e
hoangtuyen <hoangtuyen1106@gmail.com>
2019-04-27 21:57:55 +0700

    Update comment_form.php

--- includes/comment_form.php
+++ includes/comment_form.php
@@ -27,7 +27,7 @@
       $stmt = $dbc->prepare($query);
 
       //gan tham so cho cau lenh prepare
-      $stmt->bind_param('isss', $tid, $binhLuanTacGia, $binhLuanEmail, $binhLuanNoiDung);
+      $stmt->bind_param('isss', $id, $binhLuanTacGia, $binhLuanEmail, $binhLuanNoiDung);
 
       //cho chay cau lenh prepare
       $stmt->execute();
@@ -48,56 +48,41 @@
 ?>
 
 <hr>
-<h3>4 comments</h3>
+<?php 
+  $query = "SELECT count(binhluan_id) FROM binhluan";
+  if($stmt = $dbc->query($query)){
+    if($stmt->num_rows == 1) {
+      list($count) = $stmt->fetch_array(MYSQLI_NUM);
+      echo "<h3>{$count} bình luận</h3>";
+    } else{
+      echo "<h3>0 bình luận</h3>";
+    }
+  }
+?>
+<?php 
+//hien thi comment
+  $query = "SELECT binhluan_id, binhluan_tacgia, binhluan_noidung, DATE_FORMAT(binhluan_ngay,  '%b %d, %y') AS date FROM binhluan";
+  if($stmt = $dbc->query($query)){
+    if($stmt->num_rows > 0) {
+      //neu co comment thi hien thi 
+      while (list($id, $tacgia, $noidung, $ngay) = $stmt->fetch_array(MYSQLI_NUM)) {
+        echo "
+          <div class='media mb-3'>
+            <div class='media-body'>
+              <h4 class='mt-1 mb-0 mr-3'>{$tacgia}</h4>
+              <p class='mt-3 mb-2'>{$noidung}</p>
+              <time class='timeago text-muted' datetime='{$ngay}'></time>
+            </div>
+          </div>
+        ";
+      }
+    }
+  }
 
-<div class="media mb-3">
-  <div class="text-center">
-    <img class="mr-3 rounded-circle" src="img/avatars/3.png" alt="Lucy" width="100" height="100">
-    <h6 class="mt-1 mb-0 mr-3">Lucy</h6>
-  </div>
-  <div class="media-body">
-    <p class="mt-3 mb-2">Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.</p>
-    <time class="timeago text-muted" datetime="2017-12-03 20:00" data-tid="2">1 year ago</time>
-    <a class="float-right" href="#"><svg class="svg-inline--fa fa-reply fa-w-16" aria-hidden="true" data-prefix="fa" data-icon="reply" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg=""><path fill="currentColor" d="M8.309 189.836L184.313 37.851C199.719 24.546 224 35.347 224 56.015v80.053c160.629 1.839 288 34.032 288 186.258 0 61.441-39.581 122.309-83.333 154.132-13.653 9.931-33.111-2.533-28.077-18.631 45.344-145.012-21.507-183.51-176.59-185.742V360c0 20.7-24.3 31.453-39.687 18.164l-176.004-152c-11.071-9.562-11.086-26.753 0-36.328z"></path></svg><!-- <span class="fa fa-reply"></span> --> Reply</a>
 
-    <div class="media mt-3">
-      <div class="pr-3 text-center">
-        <img class="mr-3 rounded-circle" src="img/avatars/2.png" alt="John" width="100" height="100">
-        <h6 class="mt-1 mb-0 mr-3">John</h6>
-      </div>
-      <div class="media-body">
-        <p class="mt-3 mb-2">Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.</p>
-        <time class="timeago text-muted" datetime="2017-12-14 19:00" data-tid="3">1 year ago</time>
-        <a class="float-right" href="#"><svg class="svg-inline--fa fa-reply fa-w-16" aria-hidden="true" data-prefix="fa" data-icon="reply" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg=""><path fill="currentColor" d="M8.309 189.836L184.313 37.851C199.719 24.546 224 35.347 224 56.015v80.053c160.629 1.839 288 34.032 288 186.258 0 61.441-39.581 122.309-83.333 154.132-13.653 9.931-33.111-2.533-28.077-18.631 45.344-145.012-21.507-183.51-176.59-185.742V360c0 20.7-24.3 31.453-39.687 18.164l-176.004-152c-11.071-9.562-11.086-26.753 0-36.328z"></path></svg><!-- <span class="fa fa-reply"></span> --> Reply</a>
-      </div>
-    </div>
-  </div>
-</div>
-<div class="media mb-3">
-  <div class="text-center">
-    <img class="mr-3 rounded-circle" src="img/avatars/1.png" alt="Kim" width="100" height="100">
-    <h6 class="mt-1 mb-0 mr-3">Kim</h6>
-  </div>
-  <div class="media-body">
-    <p class="mt-3 mb-2">Vestibulum volutpat pretium libero. Cras id dui. Aenean ut eros et nisl sagittis vestibulum. Nullam nulla eros, ultricies sit amet, nonummy id, imperdiet feugiat, pede. Sed lectus. Donec mollis hendrerit risus. Phasellus nec sem in justo pellentesque facilisis.</p>
-    <time class="timeago text-muted" datetime="2017-11-20 20:00" data-tid="4">1 year ago</time>
-    <a class="float-right" href="#"><svg class="svg-inline--fa fa-reply fa-w-16" aria-hidden="true" data-prefix="fa" data-icon="reply" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg=""><path fill="currentColor" d="M8.309 189.836L184.313 37.851C199.719 24.546 224 35.347 224 56.015v80.053c160.629 1.839 288 34.032 288 186.258 0 61.441-39.581 122.309-83.333 154.132-13.653 9.931-33.111-2.533-28.077-18.631 45.344-145.012-21.507-183.51-176.59-185.742V360c0 20.7-24.3 31.453-39.687 18.164l-176.004-152c-11.071-9.562-11.086-26.753 0-36.328z"></path></svg><!-- <span class="fa fa-reply"></span> --> Reply</a>
-  </div>
-</div>
-<div class="media mb-3">
-  <div class="text-center">
-    <img class="mr-3 rounded-circle" src="img/avatars/4.png" alt="Paula" width="100" height="100">
-    <h6 class="mt-1 mb-0 mr-3">Paula</h6>
-  </div>
-  <div class="media-body">
-    <p class="mt-3 mb-2">Nam pretium turpis et arcu. Duis arcu tortor, suscipit eget, imperdiet nec, imperdiet iaculis, ipsum. Sed aliquam ultrices mauris. Integer ante arcu, accumsan a, consectetuer eget, posuere ut, mauris. Praesent adipiscing. Phasellus ullamcorper ipsum rutrum nunc. Nunc nonummy metus.</p>
-    <time class="timeago text-muted" datetime="2017-11-05 20:00" data-tid="5">1 year ago</time>
-    <a class="float-right" href="#"><svg class="svg-inline--fa fa-reply fa-w-16" aria-hidden="true" data-prefix="fa" data-icon="reply" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg=""><path fill="currentColor" d="M8.309 189.836L184.313 37.851C199.719 24.546 224 35.347 224 56.015v80.053c160.629 1.839 288 34.032 288 186.258 0 61.441-39.581 122.309-83.333 154.132-13.653 9.931-33.111-2.533-28.077-18.631 45.344-145.012-21.507-183.51-176.59-185.742V360c0 20.7-24.3 31.453-39.687 18.164l-176.004-152c-11.071-9.562-11.086-26.753 0-36.328z"></path></svg><!-- <span class="fa fa-reply"></span> --> Reply</a>
-  </div>
-</div>
-
+ ?>
 <div class="mt-5">
-  <h5>Write a response</h5>
+  <h5>Để lại bình luận</h5>
   <?php if(isset($messages)) {echo $messages;} ?>
   <form action="" method="post">
   <div class="row">
